75f95e2c70200a74b89415990f09d897952ab3f7
NAKAMURA Atsushi <nuits.jp@live.jp>
2020-05-16 17:54:41 +0900

    Add EastAsianWidthExtensionsTest.

---
+++ src/EastAsianWidthDotNet.Test/EastAsianWidthExtensionsTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+using Xunit;
+
+namespace EastAsianWidthDotNet.Test
+{
+    [Collection("Depends on CultureInfo.CurrentUICulture")]
+    public class EastAsianWidthExtensionsTest
+    {
+        [Fact]
+        public void GetWidthString()
+        {
+            CultureInfo.CurrentUICulture = CultureInfo.GetCultureInfo("ja-JP");
+            Assert.Equal(3, "1○".GetWidth());
+        }
+
+        [Fact]
+        public void GetWidthCultureInfo()
+        {
+            Assert.Equal(2, "1○".GetWidth(CultureInfo.GetCultureInfo("os-GE")));
+        }
+
+        [Fact]
+        public void GetWidthBool()
+        {
+            Assert.Equal(2, "1○".GetWidth(false));
+        }
+
+        [Fact]
+        public void IsFullWidth()
+        {
+            CultureInfo.CurrentUICulture = CultureInfo.GetCultureInfo("el-CY");
+            Assert.False('○'.IsFullWidth());
+        }
+
+        [Fact]
+        public void IsFullWidthCultureInfo()
+        {
+            Assert.True('○'.IsFullWidth(CultureInfo.GetCultureInfo("zh-TW")));
+        }
+
+        [Fact]
+        public void IsFullWidthBool()
+        {
+            Assert.False('○'.IsFullWidth(false));
+        }
+
+        [Fact]
+        public void GetKind()
+        {
+            Assert.Equal(EastAsianWidthKind.Ambiguous, '○'.GetKind());
+        }
+    }
+}
